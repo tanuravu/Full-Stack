7bfc025d02f5ef463047d826d71e10850678a187
TARANPREET <tanu04@gmail.com>
2025-03-02 17:24:03 +0530

    verify login is created

---
+++ server.js
@@ -0,0 +1,15 @@
+const express= require("express");
+const app= express();
+
+app.use(express.json());
+
+app.use("/api/users",require("./routes/user"))
+app.use("/api/blogs",require("./routes/blog"))
+app.use("/api/auth",require("./routes/auth"))
+app.use("/api/like",require("./routes/like"));
+app.use("/api/dislike",require("./routes/dislike"))
+app.use("/verify",require("./routes/verify"))
+
+app.listen(4000,()=>{
+    console.log("server started");
+})
